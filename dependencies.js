df13a8cb1f2f2cb0260725e388f28504dc2fa86d
jdaly04 <julia.m.daly@gmail.com>
2016-12-09 15:04:33 -0500

    Update .gitignore for deployment
    Update bundle.js and dependencies.js

--- dependencies.js
+++ dependencies.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(46);
+	module.exports = __webpack_require__(48);
 
 
 /***/ },
@@ -10341,7 +10341,7 @@
 
 /***/ },
 
-/***/ 46:
+/***/ 48:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
