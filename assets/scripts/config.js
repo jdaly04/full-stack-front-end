d5b6f1ba83eb61b05cc65646bc001678bad12119
jdaly04 <julia.m.daly@gmail.com>
2016-12-14 19:43:54 -0500

    Update master with heroku back-end link
    Change back-end pointer to heroku vs local
    server

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,18 +1,8 @@
 'use strict';
 
 const config = {
-  // host: 'https://calm-crag-10008.herokuapp.com/'
-  host: 'http://localhost:4741'
+   host: 'https://calm-crag-10008.herokuapp.com/'
+  // host: 'http://localhost:4741'
 };
 
 module.exports = config;
-
-
-
-// curl --include --request DELETE  http://localhost:4741/fundraisers/3 \
-// --header "Content-Type: application/json" \
-// --data '{
-//   "fundraiser": {
-//     "name": "looney"
-//   }
-// }'
